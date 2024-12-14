87ab058f7ae8a97a5d131fc08495492c8c2c81a4
Mohamed <mohamedpk@hotmail.de>
2024-12-15 00:43:08 +0100

    swap to ttk and added requirements

--- app.py
+++ app.py
@@ -2,11 +2,17 @@
 program to display csv files in a readable pdf
 """
 
-from tkinter import Tk, Label, Button
+from tkinter import Tk
+from tkinter.ttk import Button, Label, Style
+
 
 root = Tk()
 root.title("C2P")
 root.geometry("700x600")
+
+style = Style()
+style.theme_use("clam")
+
 
 def convert():
     print("...°°°...°°°...")
@@ -14,14 +20,10 @@ def convert():
 
 
 title = Label(root, text="CSV to PDF Converter", font=("Arial", 22))
-
 title.pack()
 
 
-
 convert_button = Button(root, text="CONVERT TO PDF", command=convert)
-convert_button.grid(row=3, column=0)
 convert_button.pack()
-
 
 root.mainloop()
