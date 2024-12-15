b241bd38397ef69c5d50c068847592c9715c839f
Mohamed <mohamedpk@hotmail.de>
2024-12-15 19:24:19 +0100

    made half a button (select file)

--- app.py
+++ app.py
@@ -3,27 +3,41 @@ program to display csv files in a readable pdf
 """
 
 from tkinter import Tk
-from tkinter.ttk import Button, Label, Style
-
+from tkinter import Label, Button, Frame, filedialog
 
 root = Tk()
 root.title("C2P")
 root.geometry("700x600")
 
-style = Style()
-style.theme_use("clam")
+#TODO: convert the csv
+def convert_file():
+    print("test")
 
 
-def convert():
-    print("...°°°...°°°...")
+def select_file():
+    file = filedialog.askopenfilename(
+            title="Select a file",
+            filetypes=(("CSV Files", "*.csv"),("All Files", "*.*")))
+    if file:
+        file_path_label.config(text=f"Selected file: {file}")
 
 
 
-title = Label(root, text="CSV to PDF Converter", font=("Arial", 22))
+content_frame = Frame(root, pady=10)
+content_frame.pack(anchor="center")
+
+
+title = Label(content_frame, text="CSV to PDF Converter", font=("Arial", 22))
 title.pack()
 
+select_button = Button(content_frame, text="select a file", command=select_file,
+                            width=50, height=20)
+select_button.pack(pady=20)
 
-convert_button = Button(root, text="CONVERT TO PDF", command=convert)
+
+file_path_label = Label(content_frame, text="No selected file")
+
+convert_button = Button(content_frame, text="CONVERT TO PDF", command=convert_file)
 convert_button.pack()
 
 root.mainloop()
